0ba0908af17222902d8ed6439c48e66ea69560a7
spiritual-geek <swapnil_kulkarni@outlook.com>
2020-03-17 17:23:51 +0530

    Spliting the dataset in to training set and datase

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -24,5 +24,7 @@ X= onehotencoder_X.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
-
+#spliting the dataset in to training set and dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,random_state=0)
 
