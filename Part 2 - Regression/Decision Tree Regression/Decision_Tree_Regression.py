f318c6f77c49b41a2ad71c21ae7c0ab6d4ff73df
Swapnilvk <kulkarni.swapnil.v@gmail.com>
2020-04-12 06:56:51 +0530

    Decision Tree Regreesion program

--- Part 2 - Regression/Decision Tree Regression/Decision_Tree_Regression.py
+++ Part 2 - Regression/Decision Tree Regression/Decision_Tree_Regression.py
@@ -37,7 +37,7 @@ y_pred = regressor.predict([[6.5]])
 
 
 # Visualising the Decision Tree Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
