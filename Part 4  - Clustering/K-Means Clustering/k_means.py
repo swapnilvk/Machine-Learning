3157c88f5673e8ec97fa0bd51227f03c4634141b
Swapnilvk <kulkarni.swapnil.v@gmail.com>
2020-05-03 00:07:48 +0530

    Python program for Hierarchical clustering

--- Part 4  - Clustering/K-Means Clustering/k_means.py
+++ Part 4  - Clustering/K-Means Clustering/k_means.py
@@ -33,11 +33,11 @@ kmeans = KMeans(n_clusters=5,init='k-means++', n_init=10, max_iter=300,random_st
 y_means= kmeans.fit_predict(X)
 
 #Visualizing the clusters
-plt.scatter(X[y_means==0,0], X[y_means==0,1], s=100, c='red', label='Cluster 1')
-plt.scatter(X[y_means==1,0], X[y_means==1,1], s=100, c='blue', label='Cluster 2')
-plt.scatter(X[y_means==2,0], X[y_means==2,1], s=100, c='green', label='Cluster 3')
-plt.scatter(X[y_means==3,0], X[y_means==3,1], s=100, c='cyan', label='Cluster 4')
-plt.scatter(X[y_means==4,0], X[y_means==4,1], s=100, c='magenta', label='Cluster 5')
+plt.scatter(X[y_means==0,0], X[y_means==0,1], s=100, c='red', label='Careful')
+plt.scatter(X[y_means==1,0], X[y_means==1,1], s=100, c='blue', label='Standard')
+plt.scatter(X[y_means==2,0], X[y_means==2,1], s=100, c='green', label='Target')
+plt.scatter(X[y_means==3,0], X[y_means==3,1], s=100, c='cyan', label='Careless')
+plt.scatter(X[y_means==4,0], X[y_means==4,1], s=100, c='magenta', label='Sensible')
 plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s=100, c='yellow', label='Centroid')
 plt.title('Clusters of clients')
 plt.xlabel('Annual income (k$)')
